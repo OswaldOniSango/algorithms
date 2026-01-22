f5a557374408978926a69c5f11ef9b1357e227ad
Oswaldo Hernandez <oswaldo.hernandez@insite360suite.com>
2026-01-22 18:13:13 -0300

    enhance trust algorithm

--- src/main/java/org/algorithm/Trust.java
+++ src/main/java/org/algorithm/Trust.java
@@ -14,7 +14,7 @@ public class Trust {
         findTrustWorthyGroups(subgroups);
     }
 
-    public static void findTrustWorthyGroups(HashMap<String, Integer> subgroups) {
+    public static HashMap<String, Integer> findTrustWorthyGroups(HashMap<String, Integer> subgroups) {
         // Find the maximum value
         int maxValue = 0;
         for (int value : subgroups.values()) {
@@ -22,14 +22,16 @@ public class Trust {
                 maxValue = value;
             }
         }
-
+        HashMap<String, Integer> trustworthyGroups = new HashMap<>();
         // Filter the trustworthy subgroups
         for (String subgroup : subgroups.keySet()) {
             int value = subgroups.get(subgroup);
             double weight = (double) value / maxValue;
-            if (weight >= 0.7) { // Set a reliability threshold (e.g., 0.9)
-                System.out.println(subgroup + " is trustworthy with a weight of " + weight);
+            if (weight >= 0.7) {
+                trustworthyGroups.put(subgroup, value);
             }
         }
+
+        return trustworthyGroups;
     }
 }
